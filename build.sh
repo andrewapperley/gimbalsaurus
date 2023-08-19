8c6ab1ce3a09972092f7d41d338ddc898ea07188
Andrew Apperley <mail@andrewapperley.ca>
2023-08-19 02:36:16 +0000

    Added comments to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
+# Init deps
 git submodule update --init --recursive
+# Create build folder
 mkdir ./build
 cd ./build
+# Build Deps
 cmake ../libgimbal -DGBL_ENABLE_TESTS=ON -GXcode
 cmake --build .
+# Build project
+cd ../gimbalsaurus
+xcodebuild -scheme gimbalsaurus build
