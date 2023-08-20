a16606fd0adea344e0e176777703e81d9f09d13b
Andrew Apperley <mail@andrewapperley.ca>
2023-08-20 12:12:51 -0400

    brought back building dep

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ cmake ../libgimbal -DGBL_ENABLE_TESTS=ON -GXcode
 cmake --build .
 # Build project
 cd ../gimbalsaurus
+xcodebuild -scheme ALL_BUILD clean build
 xcodebuild -scheme gimbalsaurus clean build
